8fcd7dac9f6b25835ea5d56346e1b0dbdbf11a65
ayscoopy <izukfred@gmail.com>
2020-06-17 23:19:26 -0700

    have fun

--- index.js
+++ index.js
@@ -1,55 +1,53 @@
-function safelock(){
-  sl = document.getElementById('safelock').value
-  dur = document.getElementById('sl_dur').value
 
-  document.getElementById("err").innerHTML = "";
-  document.getElementById("sl_tp").innerHTML = "";
 
-  if(sl == '' || dur == ''){
-    document.getElementById("err").innerHTML = "Fields cannot be blank.";
-  }else{
-    cal = (sl*dur*13)/(100*12)
-    tp = parseInt(sl)+cal
-    document.getElementById("sl_tp").innerHTML = "Your Monthly Interest is: #" +tp.toFixed(2);
-    document.getElementById("return").innerHTML = `After ${dur} months your total money would be #${tp.toFixed(2)}`
+function calculate(){
+  clear()
+  amt = document.getElementById('amount').value;
+  dur = document.getElementById('duration').value;
+  //opt = document.getElementById('opt').checked;
+  e = document.getElementById("opt");
+  opt = e.options[e.selectedIndex].value;
+
+
+  if(opt == '1'){
+    cal = (amt*dur*13)/(100*12)
+    tp = parseInt(amt)+cal
+    if (amt=='' || dur==''){
+      document.getElementById("err").innerHTML = "Fields cannot be blank."
+    }else{
+
+      document.getElementById("msg1").innerHTML = "Your Monthly Interest is: N"+cal.toFixed(2)
+      document.getElementById("msg2").innerHTML = "After "+dur+" months your total money will be N"+tp.toFixed(2)
+    }
   }
 
+  else if(opt == '2'){
+    cal = (amt*dur*10)/(100*12)
+    tp = parseInt(amt)+cal
+    if (amt=='' || dur==''){
+      document.getElementById("err").innerHTML = "Fields cannot be blank."
+    }else{
+
+      document.getElementById("msg1").innerHTML = "Your Monthly Interest is: N"+cal.toFixed(2)
+      document.getElementById("msg2").innerHTML = "After "+dur+" months your total money will be N"+tp.toFixed(2)
+    }
 }
 
-
-function t_savings(){
-  sl = document.getElementById('t_savings').value
-  dur = document.getElementById('ts_dur').value
-
-  document.getElementById("err").innerHTML = "";
-  document.getElementById("sl_tp").innerHTML = "";
-
-  if(sl == '' || dur == ''){
-    document.getElementById("err").innerHTML = "Fields cannot be blank.";
+else if(opt == '3'){
+  cal = (amt*dur*10)/(100*12)
+  tp = parseInt(amt)+cal
+  if (amt=='' || dur==''){
+    document.getElementById("err").innerHTML = "Fields cannot be blank."
   }else{
-    cal = (sl*dur*10)/(100*12)
-    tp = parseInt(sl)+cal
-    document.getElementById("sl_tp").innerHTML = "Your Monthly Interest is: #" + tp.toFixed(2);
-    document.getElementById("return").innerHTML = `After ${dur} months your total money would be #${tp.toFixed(2)}`
-  }
 
+    document.getElementById("msg1").innerHTML = "Your Monthly Interest is: N"+cal.toFixed(2)
+    document.getElementById("msg2").innerHTML = "After "+dur+" months your total money will be N"+tp.toFixed(2)
+  }
+}
 }
 
-function piggybank(){
-  sl = document.getElementById('piggybank').value
-  dur = document.getElementById('pg_dur').value
-
-  document.getElementById("err").innerHTML = "";
-  document.getElementById("sl_tp").innerHTML = "";
-
-  if(sl == '' || dur == ''){
-    document.getElementById("err").innerHTML = "Fields cannot be blank.";
-  }else{
-    cal = (sl*dur*10)/(100*12)
-    tp = (parseInt(sl)+cal ) - cal
-    document.getElementById("sl_tp").innerHTML = "Your Monthly Interest is: #" +tp.toFixed(2);
-    document.getElementById("return").innerHTML = `After ${dur} months your total money would be #${tp.toFixed(2)}`
-  }
-
+function clear(){
+  document.getElementById("msg1").innerHTML = ''
+  document.getElementById("msg2").innerHTML =''
+  document.getElementById("err").innerHTML = ''
 }
-`After ${dur} months your total money would be ${tp.toFixed(2)}`
